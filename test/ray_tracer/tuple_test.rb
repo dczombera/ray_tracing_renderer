c8f4028967c18267ab4a8bd4dc7fc328fbf5ffa4
Dennis Czombera <milchschaumschluerfer@gmail.com>
2020-02-23 20:32:27 +0100

    Refactor tuple tests

--- test/ray_tracer/tuple_test.rb
+++ test/ray_tracer/tuple_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class TupleTest < MiniTest::Test
+  def create_vector(x, y, z)
+    RayTracer::Tuple.vector(x: x, y: y, z: z)
+  end
+
+  def create_point(x, y, z)
+    RayTracer::Tuple.point(x: x, y: y, z: z)
+  end
+
   def test_that_tuple_is_point
-    tuple = RayTracer::Tuple.new x: 4.3, y: -4.2, z: 3.1, w: 1.0
+    tuple = create_point(4.3, -4.2,3.1)
     assert_equal tuple.x, 4.3
     assert_equal tuple.y, -4.2
     assert_equal tuple.z, 3.1
@@ -12,7 +20,7 @@ class TupleTest < MiniTest::Test
   end
 
   def test_that_tuple_is_vector
-    tuple = RayTracer::Tuple.new x: 4.3, y: -4.2, z: 3.1, w: 0.0
+    tuple = create_vector(4.3, -4.2, 3.1)
     assert_equal tuple.x, 4.3
     assert_equal tuple.y, -4.2
     assert_equal tuple.z, 3.1
@@ -22,85 +30,93 @@ class TupleTest < MiniTest::Test
   end
 
   def test_that_tuple_factory_creates_point
-    point = RayTracer::Tuple.point(x: 1.0, y: 2.0, z: 3.0)
+    point = create_point(1.0, 2.0, 3.0)
     assert point.is_point?
     assert !point.is_vector?
   end
 
   def test_that_tuple_factory_creates_vector
-    vector = RayTracer::Tuple.vector(x: 1.0, y: 2.0, z: 3.0)
+    vector = create_vector(1.0, 2.0, 3.0)
     assert vector.is_vector?
     assert !vector.is_point?
   end
 
-  def test_that_tuples_are_equal
-    tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 0.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.0, y: 2.2, z: 3.0, w: 0.0)
-    assert_equal false, tuple == other_tuple
+  def test_that_points_are_not_equal
+    point = create_point(1.0, 0.00001, 3.0)
+    other_point = create_point(1.0, 0.0, 3.0)
+    assert_equal false, point == other_point
 
-    tuple = RayTracer::Tuple.new(x: 1.1, y: 2.0, z: 3.0, w: 0.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.2, y: 2.0, z: 3.0, w: 0.0)
-    assert_equal false, tuple == other_tuple
-
-    tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 0.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 2.0, w: 0.0)
-    assert_equal false, tuple == other_tuple
-
-    tuple = RayTracer::Tuple.new(x: 1.00001, y: 2.0, z: 3.0, w: 1.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 1.0)
-    assert_equal false, tuple == other_tuple
+    point = create_point(1.00001, 2.0, 3.0)
+    other_point = create_point(1.0, 2.0, 3.0)
+    assert_equal false, point == other_point
   end
 
-  def test_that_tuples_are_not_equal
-    tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 0.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 0.0)
-    assert tuple == other_tuple
+  def test_that_vectors_are_not_equal
+    vector = create_vector(1.2, 2.5, 3.4)
+    other_vector = create_vector(9.3, -0.7, 1.1)
+    assert_equal false, vector == other_vector
 
-    tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 1.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 1.0)
-    assert tuple == other_tuple
+    vector = create_vector(1.00001, 2.0, 3.0)
+    other_vector = create_vector(1.0, 2.0, 2.0)
+    assert_equal false, vector == other_vector
+  end
 
-    tuple = RayTracer::Tuple.new(x: 1.000001, y: 2.0, z: 3.0, w: 1.0)
-    other_tuple = RayTracer::Tuple.new(x: 1.0, y: 2.0, z: 3.0, w: 1.0)
-    assert tuple == other_tuple
+  def test_that_points_are_equal
+    point = create_point(1.0, 2.0, 3.0)
+    other_point = create_point(1.0, 2.0, 3.0)
+    assert point == other_point
+
+    point = create_point(1.000001, 2.0, 3.0)
+    other_point = create_point(1.0, 2.0, 3.0)
+    assert point == other_point
+  end
+
+  def test_that_vectors_are_equal
+    vector = create_vector(5.5, 3.3, 1.1)
+    other_vector = create_vector(5.5, 3.3, 1.1)
+    assert vector == other_vector
+
+    vector = create_vector(1.1, 1.000001, 1.1)
+    other_vector = create_vector(1.1, 1.0, 1.1)
+    assert vector == other_vector
   end
 
   def test_that_point_and_vector_are_added 
-    result = RayTracer::Tuple.vector(x: 1.0, y: 1.0, z: 1.0) + RayTracer::Tuple.point(x: 2.0, y: 2.0, z: 2.0)
-    expected = RayTracer::Tuple.point(x: 3.0, y: 3.0, z: 3.0)
+    result = create_vector(1.0, 1.0, 1.0) + create_point(2.0, 2.0, 2.0)
+    expected = create_point(3.0, 3.0, 3.0)
     assert_equal expected, result
   end
 
   def test_that_two_vectors_are_added
-    result = RayTracer::Tuple.vector(x: 1.0, y: 1.0, z: 1.0) + RayTracer::Tuple.vector(x: 1.0, y: 1.0, z: 2.0)
-    expected = RayTracer::Tuple.vector(x: 2.0, y: 2.0, z: 3.0)
+    result = create_vector(1.0, 1.0, 1.0) + create_vector(1.0, 1.0, 2.0)
+    expected = create_vector(2.0, 2.0, 3.0)
     assert_equal expected, result
   end
 
   def test_that_error_is_raised_when_two_points_are_added
-    point = RayTracer::Tuple.point(x: 1.2, y: 1.2, z: 1.2)
+    point = create_point(1.2, 1.2, 1.2)
     assert_raises(RayTracer::PointAdditionError) { point + point }
   end
 
   def test_that_subtraction_of_two_points_results_in_vector
-    result = RayTracer::Tuple.point(x: 2.0, y: 1.0, z: 3.0) - RayTracer::Tuple.point(x: 1.0, y: 1.0, z: 1.0)
-    expected = RayTracer::Tuple.vector(x: 1.0, y: 0.0, z: 2.0)
+    result = create_point(2.0, 1.0, 3.0) - create_point(1.0, 1.0, 1.0)
+    expected = create_vector(1.0, 0.0, 2.0)
     assert_equal expected, result
   end
 
   def test_that_subtraction_of_vector_from_point_results_in_point
-    result = RayTracer::Tuple.point(x: 2.0, y: 2.0, z: 2.0) - RayTracer::Tuple.vector(x: 1.0, y: 1.0, z: 1.0)
-    expected = RayTracer::Tuple.point(x: 1.0, y: 1.0, z: 1.0)
+    result = create_point(2.0, 2.0, 2.0) - create_vector(1.0, 1.0, 1.0)
+    expected = create_point(1.0, 1.0, 1.0)
     assert_equal expected, result
   end
 
   def test_that_subtraction_of_two_vectors_results_in_vector
-    result = RayTracer::Tuple.vector(x: 3.0, y: 3.0, z: 3.0) - RayTracer::Tuple.vector(x: 1.5, y: 1.5, z: 1.5)
-    expected = RayTracer::Tuple.vector(x: 1.5, y: 1.5, z: 1.5)
+    result = create_vector(3.0, 3.0, 3.0) - create_vector(1.5, 1.5, 1.5)
+    expected = create_vector(1.5, 1.5, 1.5)
     assert_equal expected, result
   end
 
   def test_that_error_is_raised_when_point_is_subtracted_from_vector
-    assert_raises(RayTracer::VectorPointSubtractionError) { RayTracer::Tuple.vector - RayTracer::Tuple.point }
+    assert_raises(RayTracer::VectorPointSubtractionError) { create_vector(1.0, 1.0, 1.0) - create_point(1.0, 1.0, 1.0) }
   end
 end
